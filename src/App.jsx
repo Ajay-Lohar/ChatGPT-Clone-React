91eec8e2608bfd4525b0ba4a5ca8c196e2ba491b
ajaylohar <loharajay0019@gmail.com>
2025-02-16 15:32:36 +0530

    normal project

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,60 @@
 import './App.css'
-import ChatGPTLogo from './assets/Chatgpt_logo.svg'
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import megIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
+import sendBtn from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
 
-function App() {
+
+
+function App() {  
 
   return (
-    <>
-    <div className="App flex">
-      <div className="w-1/4 h-[100vh] ">
-        <div className="upperside h-[60vh]  bg-yellow-300">
-          <div className="uppersideTop flex flex-col  justify-center">
-            <div className=' flex  border-black border-1 rounded-xl p-3 m-3'>
-            <img src={ChatGPTLogo} alt="" className="logo  h-[50px]  "/>
-            <span className="brand text-3xl ">ChatGPT</span> 
-            </div>
-            <button className=' bg-blue-300  border-black border-1 rounded-xl p-3 m-3 text-2xl'>+ New Chat</button>
-            <div className="uppersidebottom flex flex-col text-sm">
-              <button className=" p-3 m-1 border-black border-1 rounded-xl ">What is Programming ?</button>
-              <button className=" p-3 m-1 border-black border-1 rounded-xl ">How to Use Api ?</button>
-              </div> 
-          </div>
-
-        </div>
-      <div className="lowerside h-[40vh] bg-amber-600">
-        <div className='flex h-[100%]  flex-col justify-center items-center '>
-          <h1 className=" p-3 m-1 border-black border-1 rounded-xl ">Home</h1>
-          <h1 className=" p-3 m-1 border-black border-1 rounded-xl ">Saved</h1>
-          <h1 className=" p-3 m-1 border-black border-1 rounded-xl ">Upgrade To Pro</h1>
-        </div>
-
+    <div className='App'>
+    <div className="sideBar">
+      <div className="upperSide">
+            <div className="upperSideTop"><img src={gptLogo} alt="Logo" className="logo" /><span className="brand">Chat GPT</span></div>
+              <button className="midBtn"><img src={addBtn} alt="" className="addBtn" />New Chat</button>
+              <p >Devloped By Ajay Lohar</p>
+              <div className="upperSideBottom">
+                <button className="query"><img src={megIcon} alt="Query" />Wht is Programming ?</button>
+                <button className="query"><img src={megIcon} alt="Query" />How to use in Api?</button>
+               
+              </div>
       </div>
-
-      </div>
-      <div className="w-3/4 h-[100vh] bg-red-400 flex flex-col justify-center items-center">
-
-      <div className="w-200 h-[90vh] flex flex-col gap-3 bg-amber-500  ">
-        <div className='flex p-2 m-4 gap-3'>
-          <img className='h-[50px]  ' src={ChatGPTLogo} alt="" />Lorem ipsum dolor sit amet , expedita?
-
-        </div>
-        <div  className='flex p-2 m-4 gap-3'>
-          <img className='h-[50px] ' src={ChatGPTLogo} alt="" />Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae at temporibus a, ullam neque est exercitationem doloribus dolor eligendi tempore dolores iusto aut atque consectetur, dolorum id. Voluptatem quae omnis a quis at voluptas repellat inventore adipisci quo ut corrupti, maxime dicta. Autem, architecto? Alias praesentium fuga voluptatibus voluptas natus aperiam unde at totam aliquam optio voluptatem fugiat eaque quasi facere, necessitatibus error. Voluptatum ipsam nihil nobis minima est beatae dolorum modi sed culpa repudiandae, hic asperiores eum deserunt, ipsa aliquid recusandae nesciunt accusamus quas tempora officia expedita corporis ratione sunt dolor. Omnis laudantium, iure labore voluptas est repudiandae cum.?
-
-        </div>
-
-        <div className=' items-end'>
-          <input type="text" placeholder='Send Message' name="" id="" />
-          <button>Send</button>
-        </div>
-
+      <div className="lowerSide">
+        <div className="listItems"><img src={home} alt="ListItems Home" className="listItemsImg" />Home</div>
+        <div className="listItems"><img src={saved} alt="ListItems Saved" className="listItemsImg" />Saved</div>
+        <div className="listItems"><img src={rocket} alt="ListItems Pro" className="listItemsImg" />Upgrade To Pro</div>
         
       </div>
-      <div className='w-200 h-[10vh] bg-red-600 text-center '>
-       Copy Rights - Ajay Lohar &copy;
+
+    </div>
+    <div className="main">
+    <div className="chats">
+
+      <div className="chat"><img className='chatImg' src={userIcon} alt="" /><p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolore adipisci distinctio molestiae sed illum eligendi dolor. Nulla, quae adipisci!</p>
       </div>
       
-.
+      <div className="chat bot"><img className='chatImg' src={gptImgLogo} alt="" /><p className='text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi et laborum saepe sed. Laborum corrupti dolores quisquam tenetur, recusandae repudiandae dicta. Itaque corporis repellendus quod rerum. Minima perspiciatis velit molestiae sint asperiores, obcaecati ad omnis, amet perferendis ut, voluptatem doloribus reiciendis rerum dicta eveniet aspernatur similique ducimus et doloremque? Eius voluptates autem minima! Neque, voluptas? Optio minus vero ex est officiis iusto quis consequatur reiciendis deleniti iste, veniam, quae eum sequi, doloremque at possimus quisquam fuga. Veritatis odio aliquam placeat eaque repellat sapiente obcaecati sit accusantium laboriosam. Velit error, nostrum odit fuga libero, ut praesentium eaque ad excepturi, non maiores! </p>
+      </div>
+
+      </div>
+      <div className="chatFooter">
+        <div className="inp">
+          <input type="text" placeholder='Send A Message '/><button className="send"><img src={sendBtn} alt="Send" /></button>
+        </div>
+        <p>Chat GPT  may produce inaccurate informltion about people, places, or facts. ChatGPT August 20 Version.</p>
       </div>
 
     </div>
 
-    </>
+
+    </div>
   )
 }
 
